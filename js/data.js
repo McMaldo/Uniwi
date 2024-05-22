f1bbb20a55f78e32e24acc64ee8385ce49728674
ligthbblue <jstachioni@gmail.com>
2024-05-22 02:10:50 -0300

    Selección y desselección de articles

--- js/data.js
+++ js/data.js
@@ -10,11 +10,24 @@ const pcComponents = [
     {id: 9,name: "Gabinete"}
 ];
 const glosario = [
-    {id: 1,word: "mierda", "desc": "para usarse en un lugar fijo"},
-    {id: 2,word: "compilar", "desc": "para usarse en un lugar fijo"},
-    {id: 3,word: "libreria", "desc": "para usarse en un lugar fijo"},
-    {id: 4,word: "cloudbook", "desc": "para usar aplicaciones de la nube"},
-    {id: 5,word: "laptop", "desc": "para usarse en incluso en movimiento"},
-    {id: 6,word: "tablet", "desc": "para usarse en un lugar fijo"},
-    {id: 7,word: "cellphone", "desc": "basados en los antiguos mp3"}
-]
+    {id: 1,word: "ASCII", desc: "Asigna una representación numérica para cada carácter, como 'a' o 'b'."},
+    {id: 2,word: "Algoritmo", desc: "Debe ser general, soportar la mayoría de las variantes que se puedan presentar en la definición del problema."},
+    {id: 3,word: "Codificación", desc: "Operación de escribir la solución del problema , en una serie de instrucciones detalladas."},
+    {id: 4,word: "Comentarios", desc: "Pueden aparecer en cualquier parte del programa."},
+    {id: 5,word: "Diagrama de flujo", desc: "Es la representación gráfica de un algoritmo"},
+    {id: 6,word: "Documentación", desc: "Es la guía o comunicación escrita es sus variadas formas, ya sea en enunciados, procedimientos, dibujos o diagramas."},
+    {id: 7,word: "Función", desc: "Es una subrutina o subprograma  se presenta como un subalgoritmo que forma parte del algoritmo principal"},
+    {id: 8,word: "Identificador", desc: "Es una serie de caracteres formados por letras, así mismo es el nombre que damos a todo lo que manipulamos dentro de un programa."},
+    {id: 9,word: "Lenguaje de programación", desc: "Sistema de símbolos y reglas que permite la construcción de programas con los que la computadora puede operar."},
+    {id: 10,word: "Mantenimiento ", desc: "Se lleva acabo después de terminado el programa, cuando se detecta que es necesario hacer algún cambio"},
+    {id: 11,word: "Operadores", desc: "Son símbolos que indica al compilador que realice operaciones lógicas o matemáticas específicas."},
+    {id: 12,word: "Operaciones primitivas elementales", desc: "Son acciones que se ejecutan sobre los datos para transformarlos en información."},
+    {id: 13,word: "Palabras reservadas", desc: "Son palabras que tienen un significado especial para el lenguaje y no se pueden utilizar como identificadores."},
+    {id: 14,word: "Programación estructurada", desc: "Método disciplinado de escribir programas que sean claros, que se demuestren que son correctos y fáciles de modificar."},
+    {id: 15,word: "Prueba y Depuración", desc: "Los errores humanos dentro de la programación  son  El proceso de identificar y eliminar errores, para dar paso a una solución sin errores se le llama depuración."},
+    {id: 16,word: "Pseudocódigo", desc: "Es la representación narrativa de los pasos que debe seguir un algoritmo para dar solución a un problema determinado. El pseudocódigo utiliza palabras que indican el proceso a realizar."},
+    {id: 17,word: "Tipos de datos", desc: "Todos los datos tienen un tipo asociado con ellos. Un dato puede ser un simple carácter, tal como b, un valor entero tal como 35. El tipo de dato determina la naturaleza del conjunto de valores que puede tomar una variable."},
+    {id: 18,word: "Variables", desc: "Una variable es una posición de memoria donde se puede almacenar una valor que se usará en el programa."},
+    {id: 19,word: "API", desc: "Es la sigla en inglés de Interfaz de Programación de Aplicaciones. Esta consiste un conjunto de reglas y protocolos utilizados para construir aplicaciones de software."},
+    {id: 20,word: "Argumento", desc: "También se los suele nombrar como 'arg'. Un argumento es un número, una cadena de texto o algún otro valor que sea necesario para que se ejecute un comando o una función."}
+];
--- js/game.js
+++ js/game.js
@@ -1,34 +1,66 @@
 const imageList = document.querySelector("#imageList");
 const wordList = document.querySelector("#wordList");
 let selecteds = {id:0,name:""};
-let clicksCounter = 0;
+let clicksCounter = {s1:false,s2:false};
+let idanterior1 = 0;
+let idanterior2 = 0;
+
 // funcion para verificar la union de los articulos
 function verificar(){
-    console.log(clicksCounter);
-    if(clicksCounter%2==0){
-        console.log(selecteds);
-        pcComponents.forEach((e)=>(selecteds == e ? console.log("bien") : console.log("mal")));
+    // console.log(clicksCounter);
+    if(clicksCounter.s1 && clicksCounter.s2){
+        // console.log(selecteds);
+        // pcComponents.forEach((e)=>(console.log(e)));
+        let elemento = false
+        pcComponents.map(e =>(selecteds.id == e.id  && selecteds.name == e.name ? elemento = true : false ));
+        console.log(elemento)
     }
 }
 function unir(id){
     let selected = document.getElementById(id);
-    let otros = imageList.querySelectorAll(".selected");
-    (selected.classList.contains("selected") ? selected.classList.remove("selected") : selected.classList.add("selected"));
-    if (otros.length <= 1){
-        otros.forEach(e => e.classList.remove("selected"));
+    let otros = imageList.querySelector(".selected");
+    if (otros){
+        otros.classList.remove("selected");
+        if(selected.id==idanterior1){
+            selected.classList.remove("selected")
+            clicksCounter.s1 = false;
+            console.log(clicksCounter)
+        }//el else se puede sacar, es solo para ver el valor de clikccounbter
+        else{
+            selected.classList.add("selected")
+            console.log(clicksCounter)
+        }
+        idanterior1 = selected.id
+    }else{
+        selected.classList.add("selected")
+        clicksCounter.s1 = true;
+        idanterior1 = selected.id
+        console.log(clicksCounter)
     }
-    clicksCounter = clicksCounter + 1;
     selecteds.id = id;
     verificar();
 }
 function unir2(id){
-    let selectedW = document.getElementById(id);
-    let otros = wordList.querySelectorAll(".selected");
-    (selectedW.classList.contains("selected") ? selectedW.classList.remove("selected") : selectedW.classList.add("selected"));
-    if (otros.length <= 1){
-        otros.forEach(e => e.classList.remove("selected"));
+    let selected = document.getElementById(id);
+    let otros = wordList.querySelector(".selected");
+    if (otros){
+        otros.classList.remove("selected");
+        if(selected.id==idanterior2){
+            selected.classList.remove("selected")
+            clicksCounter.s2 = false;
+            console.log(clicksCounter)
+        }//el else se puede sacar, es solo para ver el valor de clikccounbter
+        else{
+            selected.classList.add("selected")
+            console.log(clicksCounter)
+        }
+        idanterior2 = selected.id
+    }else{
+        selected.classList.add("selected")
+        clicksCounter.s2 = true;
+        idanterior2 = selected.id
+        console.log(clicksCounter)
     }
-    clicksCounter = clicksCounter + 1;
     selecteds.name = id;
     verificar();
 }
