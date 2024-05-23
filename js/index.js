9e0c0b7bf6f4e315dfbc4a27d2282ec9bf52b5b2
McMaldo <maldonado.ignacio.pablo@gmail.com>
2024-05-22 21:34:37 -0300

    mejoras en css

--- js/index.js
+++ js/index.js
@@ -1,13 +1,28 @@
 let glosarioSect = document.getElementById("glosario");
+let idAnterior = 0;
 function expand(id){
+    let li = document.querySelectorAll(`#glosario > li.selected`);
+    if(li){
+        li.forEach(e=>e.classList.remove("selected"));
+    }
     let otros = document.querySelectorAll(`#glosario > li > div`);
+    let selected = document.getElementById(id);
     if(otros){
         otros.forEach(e=>e.remove());
+        if(id==idAnterior){
+            selected.classList.remove("selected")
+        }else{
+            selected.classList.add("selected")
+            let descLi = document.createElement("div");
+            glosario.forEach(e=>(Number(e.id) == Number(id) ?  descLi.innerText = e.desc : false));
+            selected.appendChild(descLi);
+        }
+    }else{
+        selected.classList.add("selected")
+        let descLi = document.createElement("div");
+        glosario.forEach(e=>(Number(e.id) == Number(id) ?  descLi.innerText = e.desc : false));
+        selected.appendChild(descLi);
     }
-    let wordLi = document.getElementById(id);
-    let descLi = document.createElement("div");
-    glosario.forEach(e=>(Number(e.id) == Number(id) ?  descLi.innerText = e.desc : false));
-    wordLi.appendChild(descLi);
 }
 glosario.forEach(({id,word})=>{
     let wordLi = document.createElement("li");
